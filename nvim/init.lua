0949b437041feb9c151921cc67f5fe3c03d520e6
Reid Hansen <rhansen@influxdata.com>
2021-11-16 09:39:45 -0600

    Add plugins and format lua files

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,13 +1,19 @@
 -- Plugins
-require('plugins')
-require('key_bindings')
+require("plugins")
+require("key_bindings")
 
--- Global Settings 
-vim.cmd[[colorscheme tokyonight]]
+-- Global Settings
+vim.cmd([[colorscheme tokyonight]])
 vim.o.termguicolors = true
-vim.o.background = 'dark'
-vim.o.syntax = 'on' 
-vim.o.completeopt = 'menuone,noselect'
+vim.o.background = "dark"
+vim.o.syntax = "on"
+vim.o.completeopt = "menuone,noselect"
 vim.o.number = true
-vim.o.relativenumber = true 
+vim.o.relativenumber = true
 
+vim.cmd([[
+    augroup fmt
+        autocmd!
+	autocmd BufWritePre * undojoin | Neoformat
+    augroup END
+]])
--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -3,64 +3,73 @@
 -- Only required if you have packer configured as `opt`
 -- vim.cmd [[packadd packer.nvim]]
 
-return require('packer').startup(function()
-  use 'wbthomason/packer.nvim'
-  use 'folke/tokyonight.nvim'
+return require("packer").startup(function()
+	use("wbthomason/packer.nvim")
+	use("folke/tokyonight.nvim")
+	use("sbdchd/neoformat")
 
-    use {
-      	'neovim/nvim-lspconfig',
-      	config = function() require'plugins.lsp_config' end
-    }
+	use({
+		"neovim/nvim-lspconfig",
+		config = function()
+			require("plugins.lsp_config")
+		end,
+	})
 
-  use 'hrsh7th/nvim-cmp' -- Autocompletion plugin
-  use 'hrsh7th/cmp-nvim-lsp' -- LSP source for nvim-cmp
-  use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
-  use 'L3MON4D3/LuaSnip' -- Snippets plugin
+	use("hrsh7th/nvim-cmp") -- Autocompletion plugin
+	use("hrsh7th/cmp-nvim-lsp") -- LSP source for nvim-cmp
+	use("saadparwaiz1/cmp_luasnip") -- Snippets source for nvim-cmp
+	use("L3MON4D3/LuaSnip") -- Snippets plugin
 
-  use {
-      	'kyazdani42/nvim-tree.lua',
-      	requires = 'kyazdani42/nvim-web-devicons',
-      	config = function() require'nvim-tree'.setup {} end
-    }
+	use({
+		"kyazdani42/nvim-tree.lua",
+		requires = "kyazdani42/nvim-web-devicons",
+		config = function()
+			require("nvim-tree").setup({})
+		end,
+	})
 
-  use {
-      	'nvim-telescope/telescope.nvim',
-      	requires = { {'nvim-lua/plenary.nvim'} }
-    }
+	use({
+		"nvim-telescope/telescope.nvim",
+		requires = { { "nvim-lua/plenary.nvim" } },
+	})
 
+	use({
+		"nvim-treesitter/nvim-treesitter",
+		run = ":TSUpdate",
+		config = function()
+			require("plugins.treesitter_config")
+		end,
+	})
 
-  use { 
-	'nvim-treesitter/nvim-treesitter', 
-	run = ':TSUpdate',
-	config = function() require'plugins.treesitter_config' end
-    }
+	use({
+		"nvim-lualine/lualine.nvim",
+		requires = { "kyazdani42/nvim-web-devicons", opt = true },
+		config = function()
+			require("lualine").setup({
+				options = {
+					theme = "tokyonight",
+				},
+			})
+		end,
+	})
 
-    use {
-      'nvim-lualine/lualine.nvim',
-      requires = {'kyazdani42/nvim-web-devicons', opt = true},
-      config = function()
-        require('lualine').setup {
-          options = {
-            theme = 'tokyonight'
-          }
-        }
-      end
-    }
+	use({
+		"tanvirtin/vgit.nvim",
+		event = "BufWinEnter",
+		requires = {
+			"nvim-lua/plenary.nvim",
+		},
+		config = function()
+			require("vgit").setup({
+				hls = require("vgit").themes.tokyonight,
+			})
+		end,
+	})
 
-    use {
-        'tanvirtin/vgit.nvim',
-        event = 'BufWinEnter',
-        requires = {
-            'nvim-lua/plenary.nvim',
-        },
-        config = function()
-            require('vgit').setup {
-		hls = require('vgit').themes.tokyonight,
-	    }
-        end,
-    }
-
-
-
+	use({
+		"pwntester/octo.nvim",
+		config = function()
+			require("octo").setup()
+		end,
+	})
 end)
-
